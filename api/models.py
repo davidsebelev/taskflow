5e193c1eddf76f5c34b307c68cfd52350a8ad449
Давид <david.sebelev@gmail.com>
2026-04-18 00:04:09 +0500

    Made api folder in backend. Then added 3 custom models and 1 default model. First 3 models is Category, Task and Comment, and the last one is User.And after this completed python manage.py migrate command in cli

---
+++ api/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default = True)
+    completed = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    task  = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.text[:20]
+
